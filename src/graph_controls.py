32eed73aec056431a7aaf5f230c6b5e77cb287b4
Shahadate Rezvy <53818028+Shahadate-Rezvy@users.noreply.github.com>
2021-08-12 22:48:11 +0100

    Update graph_controls.py

--- src/graph_controls.py
+++ src/graph_controls.py
@@ -24,34 +24,32 @@ def graph_controls(chart_type, df, dropdown_options, template):
             x_values = st.sidebar.selectbox('X axis', index=length_of_options,options=dropdown_options)
             y_values = st.sidebar.selectbox('Y axis',index=length_of_options, options=dropdown_options)
             color_value = st.sidebar.selectbox("Color", index=length_of_options,options=dropdown_options)
-            #symbol_value = st.sidebar.selectbox("Symbol",index=length_of_options, options=dropdown_options)
-            #size_value = st.sidebar.selectbox("Size", index=length_of_options,options=dropdown_options)
+            symbol_value = st.sidebar.selectbox("Symbol",index=length_of_options, options=dropdown_options)
+            size_value = st.sidebar.selectbox("Size", index=length_of_options,options=dropdown_options)
             #hover_name_value = st.sidebar.selectbox("Hover name", index=length_of_options,options=dropdown_options)
-            #facet_row_value = st.sidebar.selectbox("Facet row",index=length_of_options, options=dropdown_options,)
+            facet_row_value = st.sidebar.selectbox("Facet row",index=length_of_options, options=dropdown_options,)
             facet_column_value = st.sidebar.selectbox("Facet column", index=length_of_options,
                                                       options=dropdown_options)
             #marginalx = st.sidebar.selectbox("Marginal X", index=2,options=['rug', 'box', None,
-            #                                                             'violin', 'histogram'])
-            #marginaly = st.sidebar.selectbox("Marginal Y", index=2,options=['rug', 'box', None,
-            #                                                             'violin', 'histogram'])
-            log_x = st.sidebar.selectbox('Log axis on x', options=[False,True])
-            log_y = st.sidebar.selectbox('Log axis on y', options=[False,True])
-            title = st.sidebar.text_input(label='Title of chart')
+                                                                         #'violin', 'histogram'])
+           # marginaly = st.sidebar.selectbox("Marginal Y", index=2,options=['rug', 'box', None,
+                                                                         #'violin', 'histogram'])
+            #log_x = st.sidebar.selectbox('Log axis on x', options=[True, False])
+            #log_y = st.sidebar.selectbox('Log axis on y', options=[True, False])
+            #title = st.sidebar.text_input(label='Title of chart')
             plot = px.scatter(data_frame=df,
                               x=x_values,
                               y=y_values,
                               color=color_value,
-                              #symbol=symbol_value,
-                              #size=size_value,
+                              symbol=symbol_value,
+                              size=size_value,
                               #hover_name=hover_name_value,
-                              #facet_row=facet_row_value,
+                              facet_row=facet_row_value,
                               facet_col=facet_column_value,
-                              log_x=log_x, log_y=log_y,
-                              #marginal_y=marginaly, marginal_x=marginalx,
-                              template=template, title=title)
-
-        except Exception as e:
-            print(e)
+                              #log_x=log_x, log_y=log_y,marginal_y=marginaly, marginal_x=marginalx,
+                              #template=template,
+                              #title=title
+                             )
 
     if chart_type == 'Histogram':
         st.sidebar.subheader("Histogram Settings")
